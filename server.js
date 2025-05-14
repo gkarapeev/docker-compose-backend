040e059b25027fbc841163b16ace9589752dfd72
Georgi Karapeev <gkarapeev@activtrades.com>
2025-05-14 17:41:20 +0300

    add cors

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
+const corsOptions = {
+  origin: 'http://localhost',  // Allow only your frontend's URL
+  methods: ['GET', 'POST'],  // Limit to specific methods if needed
+};
+
+app.use(cors(corsOptions));
 
 app.get('/api/hello', (req, res) => {
 	res.json({ message: 'Hello, World!' });
